8e5e09db10c437eae6685ed387e1b734f0092068
Shadowwfall <prakharbusy7878@gmail.com>
2026-06-16 17:44:46 +0530

    displaying a single article

--- main.go
+++ main.go
@@ -18,6 +18,9 @@ func main() {
 	// Handle Index
 	router.GET("/", showIndexPage)
 
+	// Handle GET requests at /article/view/some_article_id
+	router.GET("/article/view/:article_id", getArticle)
+
 	// Start serving the application
 	router.Run()
 }
